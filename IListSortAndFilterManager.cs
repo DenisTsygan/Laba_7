bb3bd8cfe4802fd8341d0f28c0d93c7281659564
DenisTsygan <denis.www.denis7@gmail.com>
2023-11-12 22:19:25 +0200

    update to Laba_9

---
+++ IListSortAndFilterManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace Laba_7
+{
+    public class IListSortAndFilterManager
+    {
+        public delegate bool CompareDelegate<T>(T left, T right);
+        public delegate bool SearchDelegate<T>(T obj, string searchValue);
+
+        /// <summary>
+        /// Sort by field in IList<<typeparamref name="T"/>> 
+        /// </summary>
+        /// <typeparam name="T">class of objects</typeparam>
+        /// <param name="colectionObjects">colection objects</param>
+        /// <param name="compareDelegate">compare delegate</param>
+        public static void SortByField<T>(IList<T> colectionObjects, CompareDelegate<T> compareDelegate)
+        {
+            for (int i = 0; i < colectionObjects.Count-1; i++)
+            {
+                for (int j = 0; j < colectionObjects.Count-1; j++)
+                {
+                    if (compareDelegate(colectionObjects[j], colectionObjects[j + 1]))
+                    {
+                        (colectionObjects[j], colectionObjects[j + 1]) = (colectionObjects[j + 1], colectionObjects[j]);
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Filter in in IList<<typeparamref name="T"/>> by field
+        /// </summary>
+        /// <typeparam name="T">class of objects</typeparam>
+        /// <param name="colectionObjects">colection objects</param>
+        /// <param name="searchValue"> search value</param>
+        /// <param name="searchDelegate"> search delegate</param>
+        /// <returns></returns>
+        public static IList<T> FilterByField<T>(IList<T> colectionObjects , string searchValue, SearchDelegate<T> searchDelegate)
+        {
+            IList<T> temp = new List<T>();
+            for (int i = 0; i < colectionObjects.Count ; i++)
+            {
+               if(searchDelegate(colectionObjects[i], searchValue))
+               {
+                    temp.Add(colectionObjects[i]);
+               }
+            }
+            return temp;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,43 +7,60 @@ namespace Laba_7
     {
         static void Main(string[] args)
         {
-             Comment comment1 = new Comment(uidAuthor: "123egr33grfddfg", nameAuthor: "ded", dateSending: new DateTime(1234, 11, 15, 10, 4, 43), message: "gj!");
-             Comment comment2 = new Comment(uidAuthor: "123egr33grfddfg", nameAuthor: "ded", dateSending: new DateTime(2004, 11, 15, 11, 4, 43), message: "hello....");
-             Comment comment3 = new Comment(uidAuthor: "y50no508ebefdd333", nameAuthor: "molli", dateSending: new DateTime(2005, 11, 15, 3, 4, 37), message: "nice to meet you");
-             Comment comment4 = new Comment(uidAuthor: "nbkdf9-n=234_33", nameAuthor: "boris", dateSending: new DateTime(2004, 11, 15, 1, 4, 43), message: "its was magic");
-             Comment comment5 = new Comment(uidAuthor: "y50no508ebefdd333", nameAuthor: "molli", dateSending: new DateTime(2004, 11, 15, 1, 4, 43), message: "well done");
+            Comment comment1 = new Comment(uidAuthor: "123egr33grfddfg", nameAuthor: "ded", dateSending: new DateTime(1234, 11, 15, 10, 4, 43), message: "gj!");
+            Comment comment2 = new Comment(uidAuthor: "123egr33grfddfg", nameAuthor: "ded", dateSending: new DateTime(2004, 11, 15, 11, 4, 43), message: "hello....");
+            Comment comment3 = new Comment(uidAuthor: "y50no508ebefdd333", nameAuthor: "molli", dateSending: new DateTime(2005, 11, 15, 3, 4, 37), message: "nice to meet you");
+            Comment comment4 = new Comment(uidAuthor: "nbkdf9-n=234_33", nameAuthor: "boris", dateSending: new DateTime(2004, 11, 15, 1, 4, 43), message: "its was magic");
+            Comment comment5 = new Comment(uidAuthor: "y50no508ebefdd333", nameAuthor: "molli", dateSending: new DateTime(2004, 11, 15, 1, 4, 43), message: "well done");
 
-             Comments commments = new Comments(comment1, comment4, comment5, comment2, comment3);
-            //Serialization JSON
+            Comments commments = new Comments(comment1, comment4, comment5, comment2, comment3);
+            Console.WriteLine("Init list:");
+            Console.WriteLine(commments);
+            IListSortAndFilterManager.SortByField<Comment>(commments.GetList(), Comment.OrderByAuthorName);
+            Console.WriteLine("After sort by AuthorName in ascending order:");
+            Console.WriteLine(commments);
             
-            FileManager.SerializationToJSON(commments.GetList(), "comments.json"); 
-            
-            //Deserialization JSON
-            
-            List<Comment> commentsFromFileJSON =(List<Comment>)FileManager.DeserializationFromJSON("comments.json",new List<Comment>());
-            Console.WriteLine("Comments from file json:\n");
-            foreach (var comment in commentsFromFileJSON)
+            IListSortAndFilterManager.SortByField<Comment>(commments.GetList(), Comment.OrderByDescendingDateSending);
+            Console.WriteLine("After sort by DateSending in descending order:");
+            Console.WriteLine(commments);
+
+            //Lambda
+            IListSortAndFilterManager.SortByField<Comment>(commments.GetList(), (Comment left, Comment right)=> DateTime.Compare(left.DateSending, right.DateSending) > 0);
+            Console.WriteLine("After sort by DateSending in ascending order:");
+            Console.WriteLine(commments);
+            //Lambda end
+
+            List< Comment > newComments1= (List<Comment>) IListSortAndFilterManager.FilterByField<Comment>(commments.GetList(), "y50no508ebefdd333", Comment.FilterByAuthorUid);
+            Console.WriteLine("Filter by AuthorUid (y50no508ebefdd333):\n");
+            foreach (var item in newComments1)
             {
-                Console.WriteLine(comment);
-                Console.WriteLine($"AuthorInfo.Uid={comment.AuthorInfo.Uid}");
-                Console.WriteLine("------------------------");
+                Console.WriteLine(item+"\n");
             }
-            
-            //Serialization Binary
-            
-            FileManager.SerializationToBinary(commments.GetList(), "comments.bin"); 
-            
-            //Deserialization Binary
-            
-            List<Comment> commentsFromFileBinary = (List<Comment>)FileManager.DeserializationFromBinary("comments.bin");
-            Console.WriteLine("Comments from file binary:\n");
-            foreach (var comment in commentsFromFileBinary)
+
+            //Lambda
+            List<Comment> newComments2 = (List<Comment>)IListSortAndFilterManager.FilterByField<Comment>(commments.GetList(), 
+                "W",
+                (Comment comment,string searchValue)=>comment.Message.Contains(searchValue,StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine("Filter by Message (W):\n");
+            foreach (var item in newComments2)
             {
-                Console.WriteLine(comment);
-                Console.WriteLine($"AuthorInfo.Uid={comment.AuthorInfo.Uid}");
-                Console.WriteLine("------------------------");
+                Console.WriteLine(item + "\n");
             }
-            
+            //Lambda end
+
+            //Anonymous method
+            List<Comment> newComments3 = (List<Comment>)IListSortAndFilterManager.FilterByField<Comment>(commments.GetList(),
+                "!",
+                delegate(Comment comment, string searchValue) {
+                    return comment.Message.Contains(searchValue, StringComparison.OrdinalIgnoreCase);
+                }
+            );
+            Console.WriteLine("Filter by Message (!):\n");
+            foreach (var item in newComments3)
+            {
+                Console.WriteLine(item + "\n");
+            }
+            //Anonymous method end
         }
     }
 }
